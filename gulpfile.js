47cc975e3864a1f8938a1ff153f362525edfedff
Meghan Hebel <meghanrhebel@gmail.com>
2017-12-06 12:39:13 -0500

    First Commit - setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,40 @@
+'use strict'
+
 const gulp = require('gulp');
-const babel = require('babelify');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const notify = require('gulp-notify');
 const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 gulp.task('styles', () => {
-	return gulp.src('./dev/styles/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles'))
+    return gulp.src('./dev/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./public/styles'))
+        .pipe(reload({stream: true}));
 });
 
-gulp.task('js', () => {
-	return browserify('dev/scripts/app.js', {debug: true})
-		.transform('babelify', {
-			sourceMaps: true,
-			presets: ['es2015','react']
-		})
-		.bundle()
-		.on('error',notify.onError({
-			message: "Error: <%= error.message %>",
-			title: 'Error in JS 💀'
-		}))
-		.pipe(source('app.js'))
-		.pipe(buffer())
-		.pipe(gulp.dest('public/scripts'))
-		.pipe(reload({stream:true}));
+gulp.task('watch', () => {
+    gulp.watch('./dev/styles/**/*.scss', ['styles']);
+    gulp.watch('./dev/scripts/app.js', ['scripts']);
+    gulp.watch('*.html', reload);
 });
 
-gulp.task('bs', () => {
-	return browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	});
+gulp.task('scripts', () => {
+    gulp.src('./dev/scripts/app.js')
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(gulp.dest('./public/scripts'))
 });
 
-gulp.task('default', ['bs','js','styles'], () => {
-	gulp.watch('dev/**/*.js',['js']);
-	gulp.watch('dev/**/*.scss',['styles']);
-	gulp.watch('./public/styles/style.css',reload);
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: '.'  
+    })
 });
+
+gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
